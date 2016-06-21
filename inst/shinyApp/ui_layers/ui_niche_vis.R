b983a62144320f260af6c7494ee45a47318ca654
luismurao <luismurao@gmail.com>
2016-06-21 00:05:19 -0500

    User input to specify the ellipsoid level

--- inst/shinyApp/ui_layers/ui_niche_vis.R
+++ inst/shinyApp/ui_layers/ui_niche_vis.R
@@ -10,17 +10,25 @@ niche_view <- sidebarLayout(position = "left",
                               selectInput(inputId = "x",label = "Select the variable in the x-axis",choices = layer_names),
                               selectInput(inputId = "y",label = "Select the variable in the y-axis",choices = layer_names),
                               selectInput(inputId = "z",label = "Select the variable in the z-axis",choices = layer_names),
-                              selectInput(inputId = "gtype",label = "Choose one kind of plot",choices = c("Scatter Plot"="disp",
-                                                                                                          "Correlation Plot"="corre"),selected="corre"),
-                              downloadButton(outputId = 'dNiheCoords',label = 'Download Bios & Coords data'),
+                              selectInput(inputId = "gtype",label = "Choose one kind of plot",
+                                          choices = c("Scatter plot"="disp",
+                                                      "Ellipsoid plot"="corre"),selected="corre"),
+
                               conditionalPanel("input.gtype=='corre'",
-                                               selectInput(inputId = "fit",label = "Want to see if there is trend? Select one model",selected = "Ninguno",choices = c("Linear"="linear",
-                                                                                                                                                                       "Nothing"=NULL,
-                                                                                                                                                                       "Quadratic"="quadratic",
-                                                                                                                                                                       "Smooth"="smooth",
-                                                                                                                                                                       "Additive"="additive"),multiple = T)
+                                               numericInput("ellipsoid_vol",
+                                                            "Proportion of data to be included in the ellipoid",
+                                                            min = 0.5,max = 1,value = 0.95,step = 0.01),
+                                               selectInput(inputId = "fit",
+                                                           label = "Want to see if there is trend? Select one model",
+                                                           selected = "Ninguno",choices = c("Linear"="linear",
+                                                                                            "Nothing"=NULL,
+                                                                                            "Quadratic"="quadratic",
+                                                                                            "Smooth"="smooth",
+                                                                                            "Additive"="additive"),multiple = T)
+
                               ),
-                              checkboxInput(inputId = "ellip",value = TRUE,label = "Display nichosphera")#,
+                              checkboxInput(inputId = "ellip",value = TRUE,label = "Display nichosphera"),
+                              downloadButton(outputId = 'downloadLatLongNiche',label = 'Download Bios & Coords data')
                               #conditionalPanel(condition = "input.ellip==true", numericInput(""))
 
                             ),
