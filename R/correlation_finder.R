e02fce6e9a3af7dd47b882170178312b13d2fa96
luismurao <luismurao@gmail.com>
2016-06-27 11:20:52 -0500

    Added function correlation_finder

--- R/correlation_finder.R
+++ R/correlation_finder.R
@@ -21,54 +21,58 @@
 
 
 correlation_finder <- function(cor_mat,threshold,verbose=TRUE){
-
-  variables_cor <- function(x,threshold){
-    x <- as.numeric(x)
-    vars_pos <- which(x > threshold)
-    vars_neg <- which(x < (-1)*threshold)
-    vars <- c(vars_pos,vars_neg)
-    cors <- x[vars]
-    names(cors) <- names(cor_mat)[vars]
-    return(cors)
-  }
-
-  list_cor <- mapply(variables_cor,x=cor_mat,threshold=threshold,SIMPLIFY = FALSE)
-
-  nomvar <- names(cor_mat)
-  list_cor2 <- sapply(nomvar, function(x) list_cor[[x]][which(list_cor[[x]]!=1)])
-
-
-  nomvar2 <- nomvar
-  descriptors <- NULL
-  for(i in 1:length(list_cor2)){
-    descriptors <- c(descriptors,names(list_cor2[i]))
-    if(names(list_cor2[i]) %in% nomvar2){
-      trash <- sort(unlist(sapply(c(descriptors,
-                                     names(list_cor2[[i]])),
-                                   function(x) which(x==nomvar2))))
-      nomvar2 <- nomvar2[-trash]
+  if(is.matrix(cor_mat) || is.data.frame(cor_mat)){
+    cor_mat <- data.frame(cor_mat)
+    variables_cor <- function(x,threshold){
+      x <- as.numeric(x)
+      vars_pos <- which(x > threshold)
+      vars_neg <- which(x < (-1)*threshold)
+      vars <- c(vars_pos,vars_neg)
+      cors <- x[vars]
+      names(cors) <- names(cor_mat)[vars]
+      return(cors)
     }
-    else{
-      descriptors <- descriptors[-length(descriptors)]
+
+    list_cor <- mapply(variables_cor,x=cor_mat,threshold=threshold,SIMPLIFY = FALSE)
+
+    nomvar <- names(cor_mat)
+    list_cor2 <- sapply(nomvar, function(x) list_cor[[x]][which(list_cor[[x]]!=1)])
+
+
+    nomvar2 <- nomvar
+    descriptors <- NULL
+    for(i in 1:length(list_cor2)){
+      descriptors <- c(descriptors,names(list_cor2[i]))
+      if(names(list_cor2[i]) %in% nomvar2){
+        trash <- sort(unlist(sapply(c(descriptors,
+                                      names(list_cor2[[i]])),
+                                    function(x) which(x==nomvar2))))
+        nomvar2 <- nomvar2[-trash]
+      }
+      else{
+        descriptors <- descriptors[-length(descriptors)]
+      }
     }
-  }
 
-  cat('*****************************************************************\n\n')
-  cat(' Here is a list of variables that can summarize your niche\n')
-  cat(' information, according to the threshold of',threshold,":\n\n")
-  cat(' ',descriptors,'\n\n')
-  cat('*****************************************************************\n\n')
+    cat('*****************************************************************\n\n')
+    cat(' Here is a list of variables that can summarize your niche\n')
+    cat(' information, according to the threshold of',threshold,":\n\n")
+    cat(' ',descriptors,'\n\n')
+    cat('*****************************************************************\n\n')
 
-  if(verbose){
+    if(verbose){
 
-    cat('----------------------------------------------------------------\n\n')
-    cat('Correlation list:\n\n')
-
-    for(i in 1:dim(cor_mat)[1]){
-      cat("Variable",names(list_cor)[i],"is strongly correlated with:\n\n")
-      print(list_cor[[i]])
       cat('----------------------------------------------------------------\n\n')
+      cat('Correlation list:\n\n')
+
+      for(i in 1:dim(cor_mat)[1]){
+        cat("Variable",names(list_cor)[i],"is strongly correlated with:\n\n")
+        print(list_cor[[i]])
+        cat('----------------------------------------------------------------\n\n')
+      }
     }
+    return(descriptors)
   }
-  return(descriptors)
+  else
+    stop("cor_mat must be a matrix or a data.frame")
 }
