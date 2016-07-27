687241866d4c8676eb65dc1488cd6a3874397278
luismurao <luismurao@gmail.com>
2016-07-26 22:45:57 -0500

    Bioclim methods

--- inst/shinyApp/server_funcs/bioclim_methods.R
+++ inst/shinyApp/server_funcs/bioclim_methods.R
@@ -16,22 +16,16 @@ observe({
 # ---------------------------------------------------------------------
 
 
-bioclim_model_all <- reactive({
+bioclim_model_all <- eventReactive(input$run_bioclim_all,{
   if(!is.null(data_extraction())){
-    input$run_bioclim
-    isolate({
-      if(input$run_bioclim && input$selectMBio == "wWorld" && !is.null(rasterLayers())){
-        model_train <- bioclim(data_extraction()[,input$biosBioclim])
-        model <- predict(rasterLayers()[[input$biosBioclim]], model_train)
-        return(list(train=model_train,prediction=model))
-
-      }
-
-    })
-
+    if(input$run_bioclim_all && !is.null(rasterLayers())){
+      model_train <- bioclim(data_extraction()[,input$biosBioclim])
+      model <- predict(rasterLayers()[[input$biosBioclim]], model_train)
+      return(list(train=model_train,prediction=model))
   }
   else
     return()
+  }
 })
 
 
@@ -40,26 +34,37 @@ bioclim_model_all <- reactive({
 # ---------------------------------------------------------------------
 
 
-bioclim_model_m <- reactive({
+bioclim_model_m <- eventReactive(input$run_bioclim_m,{
   if(!is.null(data_extraction())){
-    input$run_bioclim
-    isolate({
-      if(input$run_bioclim && input$selectMBio == "mLayers" && !is.null(define_M_raster())){
-        model_train <-  bioclim(data_extraction()[,input$biosBioclim])
-        model <- predict(define_M_raster()[[input$biosBioclim]], model_train)
-        return(list(train=model_train,prediction=model))
+    if(input$run_bioclim_m  && !is.null(define_M_raster())){
+      model_train <-  bioclim(data_extraction()[,input$biosBioclim])
+      model <- predict(define_M_raster()[[input$biosBioclim]], model_train)
+      return(list(train=model_train,prediction=model))
 
-      }
-    })
-
+    }
   }
   else
     return()
 })
 
-output$bio_response <- renderPlot({
+
+output$bio_response_all <- renderPlot({
   if(!is.null(bioclim_model_all()) && input$selectMBio == "wWorld")
-    response(bioclim_model_all()$train)
-  if(!is.null(bioclim_model_m()) && input$selectMBio == "mLayers")
-    response(bioclim_model_m()$train)
+    return(response(bioclim_model_all()$train))
 })
+
+
+output$bio_response_m <- renderPlot({
+  if(!is.null(bioclim_model_m()) && input$selectMBio == "mLayers")
+    return(response(bioclim_model_m()$train))
+})
+
+output$downBiclimRas <- downloadHandler(
+  filename <- "BioclimModelNTB.asc",
+  content <- function(file){
+    if(!is.null(bioclim_model_all()) && input$selectMBio == "wWorld")
+      writeRaster(bioclim_model_all()$prediction,file)
+    if(!is.null(bioclim_model_m()) && input$selectMBio == "mLayers")
+      writeRaster(bioclim_model_m()$prediction,file)
+  }
+)
