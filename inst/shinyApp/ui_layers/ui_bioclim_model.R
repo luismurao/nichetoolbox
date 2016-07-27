8b14be0edbce98b8706b9a13b6f6c8ec15420669
luismurao <luismurao@gmail.com>
2016-07-26 22:46:58 -0500

    Bioclim ui

---
+++ inst/shinyApp/ui_layers/ui_bioclim_model.R
@@ -0,0 +1,25 @@
+# Bliclim to NicheToolBox Methods
+# ui interface
+
+
+source("load_install_pkgs.R")
+bioclim_ui <- sidebarLayout(position = "left",
+                              sidebarPanel(
+                                titlePanel("NicheToolBox"),
+                                h3("Bioclim"),
+                                busyIndicator("Computation In progress",wait = 0),
+                                selectInput("selectMBio","Select a region to run the model",
+                                            choices = c("All raster extent"="wWorld","Your shapefile of M"="mLayers")),
+                                selectInput(inputId = "biosBioclim","Select the variables",choices = NULL,multiple = TRUE),
+                                conditionalPanel("input.selectMBio == 'wWorld'",
+                                                 actionButton("run_bioclim_all",label = "Run Bioclim")
+                                                 ),
+                                conditionalPanel("input.selectMBio == 'mLayers'",
+                                                 actionButton("run_bioclim_m",label = "Run Bioclim")
+                                )
+
+                              ),
+                              mainPanel(
+                                conditionalPanel("input.selectMBio == 'wWorld'",plotOutput("bio_response_all")),
+                                conditionalPanel("input.selectMBio == 'mLayers'",plotOutput("bio_response_m"))
+                              ))
