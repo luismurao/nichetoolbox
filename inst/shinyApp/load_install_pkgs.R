4272d6183da2a8e3f9736dfa60f5903aa7ca5284
luismurao <luismurao@gmail.com>
2016-08-06 23:10:57 -0500

    filter package dependencies

--- inst/shinyApp/load_install_pkgs.R
+++ inst/shinyApp/load_install_pkgs.R
@@ -1,31 +1,27 @@
-# Check if devtools and shinysky are installed
-pkgs <- c("devtools","shinysky")
-pkgs_miss <- pkgs[!(pkgs %in% installed.packages())]
-if(length(pkgs_miss)>0L){
-  install.packages(pkgs_miss,
-                   repos = "https://cloud.r-project.org/")
-  # Shinysky outside CRAN
-  devtools::install_github("AnalytixWare/ShinySky")
-
-
-}
+# Github dependencies
 devtools::install_github("luismurao/leaflet")
+
 # NicheToolbox dependencies
 
-pkgs_ntb <- c('ggplot2',"leaflet",'plot3D','mixtools',
-              'pvclust','fpc','snow',"sqldf",'shinysky',
-              'psych','vcd','shinythemes',
-              'KernSmooth','shinysky','nichetoolbox')
+#pkgs_ntb <- c('mixtools','pvclust','fpc',
+#              'snow','shinysky','vcd',
+#              'KernSmooth')
+
+pkgs_ntb <- c("ENMGadgets","shinysky")
 
 # Missing packages
 pkgs_ntb_miss <- pkgs_ntb[!(pkgs_ntb %in% installed.packages())]
 # Install missing packages
-if(length(pkgs_ntb_miss)>0L)
-  install.packages(pkgs_ntb_miss,
-                   repos = "https://cloud.r-project.org/")
+if(length(pkgs_ntb_miss)>0L){
+  devtools::install_github("AnalytixWare/ShinySky")
+  devtools::install_github("ENMGadgets", "narayanibarve")
+
+}
+library(ENMGadgets)
+library(shinysky)
 # Load packages
 options(rgl.useNULL=TRUE)
-sapply(pkgs_ntb,function(x) library(x,character.only = TRUE))
+#sapply(pkgs_ntb,function(x) library(x,character.only = TRUE))
 #rgl.init()
 
 
